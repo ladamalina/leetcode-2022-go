b1609abc611554a1e7f9015a4c7926797bd3b58b
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-23 03:48:39 +0300

    240. Search a 2D Matrix II (medium)

---
+++ 240-medium/README.md
@@ -0,0 +1,3 @@
+# 240. Search a 2D Matrix II (medium)
+
+https://leetcode.com/problems/search-a-2d-matrix-ii/
--- 240-medium/main.go
+++ 240-medium/main.go
@@ -2,73 +2,69 @@ package main
 
 import "sort"
 
-func search(g *[][]int, target int) bool {
-	if len(*g) == 0 {
-		return false
-	}
-
-	if len((*g)[0]) == 0 {
+func searchMatrix(g [][]int, target int) bool {
+	if len(g) == 0 || len(g[0]) == 0 {
 		return false
 	}
 
 	cut := false
 
-	n1 := sort.Search(len((*g)[0]), func(i int) bool {
-		return (*g)[0][i] >= target
+	n := sort.Search(len(g[0]), func(i int) bool {
+		return g[0][i] >= target
 	})
-	if n1 < len((*g)[0]) {
-		if (*g)[0][n1] == target {
+	if n < len(g[0]) {
+		if g[0][n] == target {
 			return true
 		}
-		if n1 == 0 {
+		if n == 0 {
 			return false
 		}
-		for i := range *g {
-			(*g)[i] = (*g)[i][0:n1]
+		for i := range g {
+			g[i] = g[i][0:n]
 		}
 		cut = true
 	}
 
-	n2 := sort.Search(len((*g)[0]), func(i int) bool {
-		return (*g)[len(*g)-1][i] >= target
+	n = sort.Search(len(g[0]), func(i int) bool {
+		return g[len(g)-1][i] >= target
 	})
-	if n2 == len((*g)[0]) {
+	if n == len(g[0]) {
 		return false
 	}
-	if (*g)[len(*g)-1][n2] == target {
+	if g[len(g)-1][n] == target {
 		return true
 	}
-	if n2 > 0 {
-		for i := range *g {
-			(*g)[i] = (*g)[i][n2:]
+	if n > 0 {
+		for i := range g {
+			g[i] = g[i][n:]
 		}
 		cut = true
 	}
 
-	n3 := sort.Search(len(*g), func(i int) bool {
-		return (*g)[i][0] >= target
+	n = sort.Search(len(g), func(i int) bool {
+		return g[i][0] >= target
 	})
-	if n3 < len(*g) {
-		if (*g)[n3][0] == target {
+	if n < len(g) {
+		if g[n][0] == target {
 			return true
 		}
-		if n3 == 0 {
+		if n == 0 {
 			return false
 		}
-		*g = (*g)[0:n3]
+		g = g[0:n]
 	}
 
-	n4 := sort.Search(len(*g), func(i int) bool {
-		return (*g)[i][len((*g)[i])-1] >= target
+	n = sort.Search(len(g), func(i int) bool {
+		return g[i][len(g[i])-1] >= target
 	})
-	if n4 == len(*g) {
+	if n == len(g) {
 		return false
 	}
-	if (*g)[n4][len((*g)[n4])-1] == target {
+	if g[n][len(g[n])-1] == target {
 		return true
 	}
-	if n4 > 0 {
-		*g = (*g)[n4:]
+	if n > 0 {
+		g = g[n:]
 		cut = true
 	}
 
@@ -76,9 +72,5 @@ func search(g *[][]int, target int) bool {
 		return false
 	}
 
-	return search(g, target)
-}
-
-func searchMatrix(g [][]int, target int) bool {
-	return search(&g, target)
+	return searchMatrix(g, target)
 }
