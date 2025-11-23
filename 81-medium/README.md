0db9328dc9a9a0b1913ec73c2a63bdb7366319be
Nadezhda R. <nadezhdaryabtsova@gmail.com>
2025-11-23 03:48:20 +0300

    81. Search in Rotated Sorted Array II (medium)

---
+++ 81-medium/README.md
@@ -0,0 +1,3 @@
+# 81. Search in Rotated Sorted Array II (medium)
+
+https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
--- 81-medium/main.go
+++ 81-medium/main.go
@@ -1,18 +1,18 @@
 package main
 
-func rec(nums *[]int, x, l, r int) bool {
+func rec(nums []int, x, l, r int) bool {
 	if l == r {
-		return (*nums)[l] == x
+		return nums[l] == x
 	}
 	m := (l + r) >> 1
-	if (*nums)[m] == x {
+	if nums[m] == x {
 		return true
 	} // x != nums[m]
-	if (*nums)[l] < (*nums)[r] { // monotonic
-		if x < (*nums)[l] || x > (*nums)[r] {
+	if nums[l] < nums[r] { // monotonic
+		if x < nums[l] || x > nums[r] {
 			return false
 		}
-		if x < (*nums)[m] {
+		if x < nums[m] {
 			return rec(nums, x, l, m)
 		}
 		return rec(nums, x, m+1, r)
@@ -21,5 +21,5 @@ func rec(nums *[]int, x, l, r int) bool {
 }
 
 func search(nums []int, target int) bool {
-	return rec(&nums, target, 0, len(nums)-1)
+	return rec(nums, target, 0, len(nums)-1)
 }
